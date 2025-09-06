4fe5d7dc8f8ad41f77096ab49a9d9b48d379fe91
Elian-Cordone7 <eliancordonee@gmail.com>
2025-09-06 06:55:35 -0300

    Agregar backend y app de consola

---
+++ atm-backend/src/main/java/com/link/atm_service/controller/AtmController.java
@@ -0,0 +1,19 @@
+package com.link.atm_service.controller;
+
+import com.link.atm_service.service.AtmService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/api")
+public class AtmController {
+
+    private final AtmService atmService;
+
+
+
+}
---
+++ atm-backend/src/main/java/com/link/atm_service/model/Tarjeta.java
@@ -0,0 +1,4 @@
+package com.link.atm_service.model;
+
+public class Tarjeta {
+}
